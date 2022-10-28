7a8ba1da6207a936df1fa50dc41c4236d9bdd508
Sparrow <dfleites1234@gmail.com>
2022-10-28 15:54:05 -0500

    Creating Dates

--- starter/script.js
+++ starter/script.js
@@ -387,7 +387,6 @@ console.log(PI);
 
 console.log(Number('230_000'));
 console.log(parseInt('230_000'));
-*/
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Working with BigInt
@@ -422,3 +421,41 @@ console.log(huge + ' is REALLY big!!!');
 // Divisions
 console.log(11n / 3n);
 console.log(10 / 3);
+*/
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Creating Dates
+
+// Create a date
+const now = new Date();
+console.log(now);
+
+console.log(new Date('Oct 27 2022 17:12:17'));
+console.log(new Date('December 24, 2015'));
+console.log(new Date(account1.movementsDates[0]));
+
+console.log(new Date(2037, 10, 19, 15, 23, 5));
+console.log(new Date(2037, 10, 33));
+
+console.log(new Date(0));
+console.log(new Date(3 * 24 * 60 * 60 * 1000));
+
+// Working with dates
+const future = new Date(2037, 10, 19, 15, 23);
+console.log(future);
+console.log(future.getFullYear());
+console.log(future.getMonth());
+console.log(future.getDate());
+console.log(future.getDay());
+console.log(future.getHours());
+console.log(future.getMinutes());
+console.log(future.getSeconds());
+console.log(future.toISOString());
+console.log(future.getTime());
+
+console.log(new Date(2142278580000));
+
+console.log(Date.now());
+
+future.setFullYear(2040);
+console.log(future);
